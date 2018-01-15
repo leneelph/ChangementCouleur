f5275f655fbd027acd523026e6171f0b40e7467d
leneelph <faniloledev@gmail.com>
2018-01-15 17:52:36 +0100

    Ajouté nouvelles couleurs

--- js/changementcouleur.js
+++ js/changementcouleur.js
@@ -1,20 +1,27 @@
 document.addEventListener('keypress', function(e){
 	var touche = String.fromCharCode(e.charCode); // Récupération de la touche
 	touche = touche.toUpperCase(); // Pour gérer indifféremment minuscules et majuscules
-	
-
-	if (touche == 'R') {
-
-		// appliquer le changement de couleur de fond à toutes les div
-		var listDiv = document.getElementsByTagName('div');
-		for (var i = 0; i < listDiv.length; i++) {
-			listDiv[i].style.backgroundColor = 'red';
-		}
-
+	var couleur = "";
+	switch (touche) // associer chaque touche à une couleur
+	{
+		case 'R':
+			couleur = 'red';
+			break;
+		case 'J':
+			couleur = 'yellow';
+			break;
+		case 'V':
+			couleur = 'green';
+			break;
+		case 'B':
+			couleur = 'blue';
+			break;
+		default:
+			alert(touche + ' n\'est pas une touche gérée');
 	}
-
-	else {
-		alert(touche + ' n\'est pas une touche gérée');
+	// appliquer le changement de couleur de fond à toutes les div
+	var listDiv = document.getElementsByTagName('div');
+	for (var i = 0; i < listDiv.length; i++) {
+		listDiv[i].style.backgroundColor = couleur;
 	}
-
 })
